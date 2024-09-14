36aa36c8b93c5ca9d07938d8436885b3a79e127e
naveenmalla <s4namall@uni-trier.de>
2024-09-14 22:24:28 +0200

    updated wordcloud color and add nltk modules

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from nltk.corpus import stopwords
 
 # Download NLTK data if not already available
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('stopwords')
+nltk.download('wordnet')
 
 # Load the dataset once
 chatgpt_df = pd.read_csv('chatgpt_text.csv')
@@ -18,7 +20,7 @@ chatgpt_df = pd.read_csv('chatgpt_text.csv')
 st.title("ChatGPT Text Analysis by Topic")
 
 # Topic selection dropdown
-topics = ['world', 'sports', 'business', 'yelp', 'imdb', 'ivy', 'medical', 'finance', 'history', 'science']
+topics = [ 'sports', 'business', 'world', 'yelp', 'imdb', 'ivy', 'medical', 'finance', 'history', 'science']
 topic = st.selectbox('Select a Topic:', topics)
 
 # Combine all text for the selected topic
@@ -27,8 +29,16 @@ text_topic = ' '.join([str(text) for text in chatgpt_df[chatgpt_df['topic'] == t
 # Show word cloud
 if text_topic:
     st.header(f"Word Cloud for {topic}")
-    wordcloud = WordCloud(width=3000, height=2000, random_state=1, background_color='salmon', 
-                          colormap='Set2', collocations=False, stopwords=STOPWORDS).generate(text_topic)
+    wordcloud = WordCloud(
+        width=3000, 
+        height=2000, 
+        random_state=1, 
+        background_color='white',  
+        colormap='viridis',  
+        collocations=False, 
+        stopwords=STOPWORDS
+    ).generate(text_topic)
+    
     plot_cloud(wordcloud)
 
     # Analyze and plot n-grams (from 2-grams to 4-grams)
